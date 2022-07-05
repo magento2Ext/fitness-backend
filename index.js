2fcf7d78d04233371b7f701a691b22afa1f8b999
aman <aman@int.iqsay.com>
2022-07-05 15:52:26 +0530

    weight

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const auth = require("./middleware/auth");
 app.use(express.json())
 const API_PORT = process.env.API_PORT
 const Weight = require('./models/weight')
+const dateLib = require('date-and-time')
  
 const whitelist = [process.env.REACT_APP_URL]
 const corsOptions = {
@@ -76,6 +77,69 @@ app.post("/weight/list", auth, async(req, res) => {
 		}
 		const weight = await Weight.find({  employeeId: req.user.user_id})
 		response = webResponse(201, true, weight)  
+		res.send(response);
+		return;
+	} catch(err){   console.log(err)
+		response = webResponse(403, false, err)  
+	    res.send(response)
+		return;
+    }
+  
+}); 
+
+app.post("/weight", auth, async(req, res) => { 
+  try{ 
+		var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+
+		var oneWeekAgo = new Date();
+		oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+		var date = new Date();
+		
+		const recentWeight = await Weight.findOne({ employeeId: req.user.user_id}).sort({date:-1});
+		
+		const weightList = await Weight.find({  employeeId: req.user.user_id,
+			date: {
+				$gte: dateLib.format(oneWeekAgo,'YYYY-MM-DD'),
+				$lt: dateLib.format(date,'YYYY-MM-DD'),
+			}
+		}).sort({date:1})
+		
+		weightArray = [];
+		var i=0;
+		weightList.forEach(function(col) {
+			// Do something with each collection.				  
+			if(i == 0) {
+				var weight = {
+					'date' : col.date,
+					'weight' : col.weight,
+					'day' :  days[col.date.getDay()],
+					'difference':0,
+					'weightLine':''
+					
+				}
+			} else{
+				var difference = col.weight - weightList[i-1].weight;
+				if(difference > 0) {
+					var line = difference+" kilogram over weight."
+				} else {
+					var line = difference+" kilogram under weight."
+				}
+				var weight = {
+					'date' : col.date,
+					'weight' : col.weight,
+					'day' :  days[col.date.getDay()],
+					'difference': difference,
+					'weightLine':line,
+				}
+			}
+			weightArray.push(weight);
+			i++;		
+		});
+		var data = {};
+		data.weight_diff = weightArray
+		data.recentWeight = recentWeight
+		
+		response = webResponse(202, true, data)  
 		res.send(response);
 		return;
 	} catch(err){   console.log(err)
