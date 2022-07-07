856389847e11a0dc7baba9f9ff0409d2294651d3
aman <aman@int.iqsay.com>
2022-07-07 17:10:15 +0530

    chnges

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const config = process.env;
+const Employee = require('../models/employee')
 
 const verifyToken = (req, res, next) => {
   const token = req.body.token || req.query.token || req.headers["authorization"];
@@ -11,6 +12,13 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
+	var empId = req.user.user_id;
+	const employee = Employee.findById(empId)
+	if(!employee){
+			response = webResponse(404, false, "Employee not found.")  
+			res.send(response)
+			return;
+	}
   } catch (err) {
 	response = webResponse(401, false, "Invalid Token")  
 	res.send(response)
