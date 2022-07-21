5907adbb116e7d1056244c809d833f27ed09807b
aman <aman@int.iqsay.com>
2022-07-21 15:59:12 +0530

    chnges

--- routes/upload.js
+++ routes/upload.js
@@ -9,7 +9,7 @@ const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY
 });
-
+app.use(express.limit('4M'));
 
 
 var upload = multer({
