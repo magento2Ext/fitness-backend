312347487181e3bd0b2fbf020a416347b916aae6
Your Name <harinder.cipio.ai>
2022-08-12 19:45:50 +0530

    latest

--- routes/employee.js
+++ routes/employee.js
@@ -643,10 +643,10 @@ router.put('/update/:id',async(req,res)=> {
 			isVerified: req.body.isVerified,
 		}
 
-            await Employee.updateOne({_id: req.body.id}, {$set: data}); 
+            await Employee.updateOne({_id: req.body.id}, {$set: data}, {new: true}); 
 
 		if(req.body.isVerified) {
-			await Employee.updateOne({_id: req.body.id}, {$set: {organizationId: req.body.orgId}},  {$push: {userOrganizations: req.body.orgId}}); 
+			await Employee.updateOne({_id: req.body.id}, {$set: {organizationId: req.body.orgId}},  {$push: {userOrganizations: req.body.orgId}}, {new: true}); 
 		}
 
 		organizationRequests.updateOne({_id: req.body.reqId}, {$set: {status: req.body.isVerified ? 1 : 2}});
