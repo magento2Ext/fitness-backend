9df9fe7d9dd460e1b2c1dd7382c9d074e5f63a63
Your Name <harinder.cipio.ai>
2022-08-12 16:04:24 +0530

    latest

--- routes/chat.js
+++ routes/chat.js
@@ -223,9 +223,8 @@ router.post('/get_single_inboxes/list', auth, async(req,res) => {
 	   
 				 for(let key of messages){
 	   
-				   var other_person_id =  key.deliveredTo[0] == empId ? key.employeeId  : key.deliveredTo[0];
-				   let myId = String(other_person_id).split('"');
-				   console.log("myId", myId);
+				   var other_person_id =  key.deliveredTo[0] == empId ? String(key.employeeId)  : String(key.deliveredTo[0]);
+
 				   if(ids.indexOf(other_person_id)== -1){
 				
 				   console.log(ids);
