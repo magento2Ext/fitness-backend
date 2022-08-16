9f6ea68b1ffa0cac0cdd776fc19ff9f7fd0f4236
Your Name <harinder.cipio.ai>
2022-08-16 13:18:02 +0530

    latest

--- routes/employee.js
+++ routes/employee.js
@@ -666,7 +666,7 @@ router.put('/update/:id',async(req,res)=> {
 		organizationRequests.updateOne({_id: req.body.reqId}, {$set: {status: req.body.isVerified == "false" ? 2 : 1}});
        
 		const a1 = await Employee.findOne({_id: req.body.id})
-        response = webResponse(200, true, a1)  
+        response = webResponse(202, true, a1)  
 	    res.send(response)   
 
     }catch(err){
