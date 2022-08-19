e3f37e164271db9f3c3ddc1d34589bf08569de50
Your Name <harinder.cipio.ai>
2022-08-19 17:25:31 +0530

    latest

--- routes/upload.js
+++ routes/upload.js
@@ -25,7 +25,7 @@ var upload = multer({
             cb(null, Date.now().toString())
        },
 	    contentType: multerS3.AUTO_CONTENT_TYPE
-   })
+   }), limits: { fileSize: 52428800 } 
 })
 
 app.post('/upload', upload.single('picture'), function (req, res, next) { 
