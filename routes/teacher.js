28269a8910e43cd2122c9e34e1d60ef057c0f927
Your Name <harinder.cipio.ai>
2022-08-23 14:42:31 +0530

    latest

--- routes/teacher.js
+++ routes/teacher.js
@@ -89,7 +89,12 @@ router.post('/teachersByOrgId', auth, async(req,res) => {
 		var empId = req.user.user_id;
 		const employeeDetails = await Employee.findById(empId)
 
-		const teachers = await Teachers.find({orgId: employeeDetails.userOrganizations.length != 0 ?  employeeDetails.organizationId : '0'});
+		let query  = {};
+
+		if(employeeDetails.userOrganizations.length != 0) query = {orgId: employeeDetails.organizationId, type: 'org'}
+		else query = {type: 'admin'}
+
+		const teachers = await Teachers.find(query);
         response = webResponse(201, true, teachers)  
 		res.send(response)		
 		return;
