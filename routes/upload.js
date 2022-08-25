d3ccc02bab49b3c7a05d2f9177cdf6e39d6f3a64
Your Name <harinder.cipio.ai>
2022-08-25 18:16:15 +0530

    latest

--- routes/upload.js
+++ routes/upload.js
@@ -20,18 +20,54 @@ var upload = multer({
            cb(null, { fieldName: file.fieldname });
        },
        key: function (req, file, cb) {
+
+        const filename = `${Date.now()}_${sanitize(
+            file.originalname.replace(
+              /[`~!@#$%^&*()_|+\-=÷¿?;:'",<>{}[]\\\/]/gi,
+              '',
+            ),
+          )}`;
+      
+          var newFileName = Date.now() + "-" + filename;
+          var fullPath = 'users/profile/' + newFileName;
+          cb(null, fullPath);
+
           //console.log(file.location);
-            cb(null, Date.now().toString())
+            // cb(null, Date.now().toString())
        },
 	    contentType: multerS3.AUTO_CONTENT_TYPE
    }), limits: { fileSize: 52428800 } 
 })
 
 app.post('/upload', upload.single('picture'), function (req, res, next) { 
-	var data = {'message':'File uploaded', 'url':req.file.location}
+	var data = {'message':'File uploaded', 'url': req.file.location}
 	response = webResponse(202, true, data)  
 	res.send(response)
 	return "";
 }) 
 
  module.exports = app
+
+
+ multerS3({
+    s3: s3,
+    bucket: process.env.AWS_PROFILE_IMAGE_BUCKET,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (request, file, ab_callback) {
+      ab_callback(null, {fieldname: file.fieldname});
+    },
+    key: function (request, file, ab_callback) {
+  
+      // add hash to sanitized file name
+      const filename = `${Date.now()}_${sanitize(
+        file.originalname.replace(
+          /[`~!@#$%^&*()_|+\-=÷¿?;:'",<>{}[]\\\/]/gi,
+          '',
+        ),
+      )}`;
+  
+      var newFileName = Date.now() + "-" + filename;
+      var fullPath = 'users/profile/' + newFileName;
+      ab_callback(null, fullPath);
+    },
+  })
