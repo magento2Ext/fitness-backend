a8b3105332ddaddbc2c84169e341452fec5bed51
Your Name <harinder.cipio.ai>
2022-08-25 19:07:37 +0530

    latest

--- routes/teacher.js
+++ routes/teacher.js
@@ -420,7 +420,7 @@ router.post('/getAdminPosts', async(req,res) => {
 	}
  
 
-	let result = await Audio.aggregate([
+	let result = await Audio.aggregateOne([
 		{$match: {userId: userId}},
 		{$set: {catId: {$toObjectId: "$catId"} }},
 		{$lookup: {
