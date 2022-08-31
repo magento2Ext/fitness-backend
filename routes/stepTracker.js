a18ec3c2a8fd1af8fbcdfb48879aa233581610fa
Your Name <harinder.cipio.ai>
2022-08-31 16:35:54 +0530

    latest

--- routes/stepTracker.js
+++ routes/stepTracker.js
@@ -76,7 +76,7 @@ router.post('/resetTarget', auth, async(req,res) => {
 	let date1 = new Date(employeeDetails.date.replace(/-/g, "/"));
 	let date2 = new Date(date_2.replace(/-/g, "/"));
  
-	let difference =  date1.getTime() - date2.getTime()
+	let difference =  date2.getTime() - date1.getTime()
 
 	let days = Math.ceil(difference / (1000 * 3600 * 24));
 
