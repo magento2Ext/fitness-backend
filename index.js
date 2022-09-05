3b8e2d5c643c109c476e88951893ca31a7c35448
Your Name <harinder.cipio.ai>
2022-09-05 12:42:09 +0530

    latest

--- index.js
+++ index.js
@@ -152,16 +152,16 @@ app.post("/weight", auth, async(req, res) => {
 				let height = employeeDetails.height;
 				let weight = latestWeight.weight * 0.45359237;
 				let BMI  = (weight / ((height * height) / 10000)).toFixed(2);
-				result.innerText = BMI;
+				result.BMI = BMI;
 
 				if(BMI < 18.5){
-				statement.innerText = "Your BMI falls within the underweight range";    
+					result.innerText = "Your BMI falls within the underweight range";    
 				}else if((BMI > 18.5) && (BMI < 24.9)){
-				statement.innerText = "Your BMI falls within the normal or healthy weight range";
+					result.innerText = "Your BMI falls within the normal or healthy weight range";
 				}else if((BMI > 25) && (BMI < 29.9 )){
-				statement.innerText = "Your BMI falls within the overweight range";
+					result.innerText = "Your BMI falls within the overweight range";
 				}else{
-				statement.innerText = "Your BMI falls within the obese range";
+					result.innerText = "Your BMI falls within the obese range";
 				}
 		}
 		 
