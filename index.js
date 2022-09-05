c4e13e06b96949858fe314e5ea02f6f64cb7a3cc
Your Name <harinder.cipio.ai>
2022-09-05 14:50:08 +0530

    latest

--- index.js
+++ index.js
@@ -347,14 +347,7 @@ app.post("/weight", auth, async(req, res) => {
 			return;
 
 		}
-        
-	// } catch(err){   
-	// 	console.log(err)
-	// 	response = webResponse(403, false, err)  
-	//     res.send(response)
-	// 	return;
-    // }
-  
+          
 }
 
 ); 
