73ed5dccb1ccbbec7ea459c77e42030ae53cee72
Your Name <harinder.cipio.ai>
2022-09-20 15:02:00 +0530

    latest

--- routes/challenge.js
+++ routes/challenge.js
@@ -13,6 +13,12 @@ router.post('/create', auth, async(req, res) => {
        let empId = req.user.user_id;
        const employee = await Employee.findById(empId);
 
+       if(orgType === 'employee' && !employee.organizationId){
+        response = webResponse(202, false, 'Error saving challenge')  
+        res.send(response)
+        return;
+       }
+
        let {userId, type, title, description, pic, start, end, orgType, winners, invites, dailyStepLimit, weightType, targetWeight, targetBMI, activities} = req.body;
 
        let data = {
