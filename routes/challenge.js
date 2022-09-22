20e044d2c3f7f86725345223e1dd8b8a9f7caab5
Your Name <harinder.cipio.ai>
2022-09-22 12:29:02 +0530

    latest

--- routes/challenge.js
+++ routes/challenge.js
@@ -476,17 +476,9 @@ router.post('/challengeDetail', async(req, res) => {
              }},
              { "$lookup": {
                 "from": "activities",
-
-                  //setting variable [searchId] where your string converted to ObjectId
-                  'let': {"challengeId": {$toObjectId: "$challengeId"}}, 
-                  //search query with our [searchId] value
-                  "pipeline":[
-                    //searching [searchId] value equals your field [_id]
-                    {"$match": {"$expr":[ {"_id": "$$challengeId"}]}},
-                    //projecting only fields you reaaly need, otherwise you will store all - huge data loads
-                    {"$project":{"_id": 1}}
-
-                  ],
+                'let': {"challengeId": {$toObjectId: "$challengeId"}}, 
+                "localField": "_id",
+                "foreignField": "$$challengeId",
                 "as": "activitiesObj"
              }},
             { "$unwind": {path: "$participantsObjects", preserveNullAndEmptyArrays:true}},
