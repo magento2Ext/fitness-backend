70a0c8148d2efcca99009846a8812b4ee6bda55f
Your Name <harinder.cipio.ai>
2022-09-22 18:06:08 +0530

    latest

--- routes/challenge.js
+++ routes/challenge.js
@@ -641,7 +641,7 @@ router.post('/mindLeaderboard', auth, async(req, res) => {
              {
                 "$group": {
                 "_id": "$participants",
-                "participantsObjects": { "$push": "$participantsObject" },
+                "participantsObjects": { "$push": "$participantsObjects" },
             }}
         ]);
 
