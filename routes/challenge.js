cb297294eed7e2dc5e96982e5c42db7dd723cde6
Your Name <harinder.cipio.ai>
2022-09-22 12:37:50 +0530

    latest

--- routes/challenge.js
+++ routes/challenge.js
@@ -482,9 +482,8 @@ router.post('/challengeDetail', async(req, res) => {
                 //search query with our [searchId] value
                 "pipeline":[
                   //searching [searchId] value equals your field [_id]
-                  {"$match": {"_id": "$$searchId"}}
-                  //projecting only fields you reaaly need, otherwise you will store all - huge data loads
-          
+                  {"$match": {"$expr":[ {"_id": "$$searchId"}, {"$$searchId": "_id"}]}}
+      
 
                 ],
 
