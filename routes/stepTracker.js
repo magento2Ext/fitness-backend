f44e12f7377e449e852327d58b8a2a59dd3f9981
Your Name <harinder.cipio.ai>
2022-09-26 17:05:50 +0530

    latest

--- routes/stepTracker.js
+++ routes/stepTracker.js
@@ -10,6 +10,9 @@
  router.post('/save', auth, async(req, res) => {
 	try{ 
 
+		console.log(req.body);
+
+	 
 		var stepTarget = await EmpStepTarget.findOne({ employeeId: req.user.user_id}).sort({date:-1});
 
 		let stepTargetSteps = (stepTarget.steps + req.body.steps) <= stepTarget.step_target ?  (stepTarget.steps + req.body.steps) : stepTarget.step_target;
