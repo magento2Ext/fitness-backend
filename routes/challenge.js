4693d382d6558688c17f123b9e2abdea45a8c66b
Your Name <harinder.cipio.ai>
2022-10-13 12:57:09 +0530

    latest

--- routes/challenge.js
+++ routes/challenge.js
@@ -961,7 +961,7 @@ router.post('/weightChallengeDetail', auth, async(req, res) => {
                         
                                 for( var j = 0, len = weightArray.length; j < len; j++ ) { 
                                     var weightData = '';
-                                    console.log(weightArray[j]['day'], days[i.getDay())
+                                    console.log(weightArray[j]['day'], days[i.getDay()])
                                     if( weightArray[j]['day'] == days[i.getDay()]) {
                                         found = 1;
                                         weightData = weightArray[j];
