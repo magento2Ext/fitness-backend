36adf86c4303b7ada65050fea50d4e0298fafa55
Your Name <harinder.cipio.ai>
2022-10-20 11:58:03 +0530

    latest

--- routes/challenge.js
+++ routes/challenge.js
@@ -47,7 +47,8 @@ var job = new CronJob(
 
 router.post('/create', auth, async(req, res) => {
    try{ 
-       
+    
+       console.log(req.body)
        let mindTypes = ['yoga', 'meditation', 'mood', 'routine', 'fitness', 'mind'];
        let empId = req.user.user_id;
        const employee = await Employee.findById(empId);
